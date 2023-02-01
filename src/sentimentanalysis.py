c2b73b786cf21f88ca75c0218ff1b408ed19e93a
Koyote059 <koyote059@gmail.com>
2023-02-01 18:32:13 +0100

    Implemented. Remember to install transformers and tensorflow.

--- src/sentimentanalysis.py
+++ src/sentimentanalysis.py
@@ -1,11 +1,23 @@
-from feel_it import EmotionClassifier, SentimentClassifier
+# from feel_it import EmotionClassifier, SentimentClassifier
+from transformers import pipeline, Pipeline
+
 
 class SentimentAnalyzer:
+    _sentiment_pipeline: Pipeline
 
-    @staticmethod
-    def getScore(phrase: str) -> float:
+    def __init__(self):
+        self._sentiment_pipeline = pipeline("sentiment-analysis")
+
+    def getScore(self, phrase: str) -> float:
         """
         :param phrase: The text to be analyzed.
         :rtype: float. It's a value between -1 and 1.
         """
-        pass
+        sentiment = self._sentiment_pipeline(phrase)[0]
+        print(sentiment)
+        if sentiment['label'] == 'POSITIVE':
+            return sentiment["score"]
+        elif sentiment["label"] == 'NEGATIVE':
+            return -sentiment["score"]
+        else:
+            raise TypeError(sentiment)
