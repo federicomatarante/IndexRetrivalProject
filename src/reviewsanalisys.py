08f40d5eaba449bc2b7611925ca7e429175b5ded
Koyote059 <koyote059@gmail.com>
2023-02-02 16:19:51 +0100

    Adapted code to Sentiment Analyzer

--- src/reviewsanalisys.py
+++ src/reviewsanalisys.py
@@ -3,12 +3,14 @@ from src.sentimentanalysis import SentimentAnalyzer
 
 
 class ReviewsAnalyzer:
+    sentimentAnalyzer: SentimentAnalyzer
 
-    _sentimentAnalyzer = SentimentAnalyzer()
+    def __init__(self, sentimentAnalyzer: SentimentAnalyzer):
+        self.sentimentAnalyzer = sentimentAnalyzer
 
     def getSentiment(self, review: Review) -> float:
         """
         :param review: The review to be analyzed.
         :rtype: float. It's a value between -1 and 1.
         """
-        return self._sentimentAnalyzer.getScore(review.text)
+        return self.sentimentAnalyzer.getScore(review.text)
