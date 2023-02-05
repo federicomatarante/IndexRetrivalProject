753c5d285996e3c7cbed79237f58de41d38ada02
Koyote059 <koyote059@gmail.com>
2023-02-05 12:34:21 +0100

    Added id field to Review and to Product to allow duplicate names.

--- src/apii.py
+++ src/apii.py
@@ -1,16 +1,24 @@
+import uuid
 from dataclasses import dataclass
+
 
 @dataclass
 class Review:
     stars: int
     text: str
+    id: uuid = None
+
+    def __post_init__(self):
+        self.id = uuid.uuid4() if self.id is None else self.id
 
 
 @dataclass
 class Product:
     title: str
     link: str
-    reviews: list[Review]
+    reviews: list[Review] = None
+    id: uuid = None
 
     def __post_init__(self):
         self.reviews = [] if self.reviews is None else self.reviews
+        self.id = uuid.uuid4() if self.id is None else self.id
